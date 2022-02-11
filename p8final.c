2902913e865665156bc39bc5723ce9fdd38112e5
M Shashank <94677981+Shashank1726@users.noreply.github.com>
2022-02-11 12:33:57 +0000

    Rev

--- p8final.c
+++ p8final.c
@@ -1,41 +1,57 @@
 #include <stdio.h>
 struct _triangle
+
 {
+
 float base,altitude,area;
 };
+
 typedef struct _triangle Triangle;
 int input_n()
 {
+
   int n;
   printf("Enter the n value \n");
   scanf("%d",&n);
   return n;
+
 }
+
 Triangle input_triangle()
+
 {
   Triangle t;
   printf ("Enter base and altitude of triangle \n");
   scanf ("%f%f", &t.base, &t.altitude);
   return t;
+
 }
 void input_n_triangles(int n, Triangle t[n])
+
 {
   int i;
   for (i=0;i<n;i++)
     t[i]=input_triangle();
 }
+
 void find_area(Triangle *t)
 {
+
   t->area=0.5*t->base*t->altitude;
 }
+
 void find_areas_n(int n, Triangle t[n])
+
 {
   int i;
   for(i=0;i<n;i++)
   find_area(&t[i]);
 }
+
 Triangle find_smallest_triangle(int n, Triangle t[n])
+
 {
+
   int i;
   Triangle Smallest;
   Smallest.area = t[0].area;
@@ -49,14 +65,18 @@ Triangle find_smallest_triangle(int n, Triangle t[n])
     }    
     return Smallest;
 }
+
 void output(int n, Triangle t[n], Triangle smallest)
+
 {
     int i;
     for (i=0;i<n;i++)
     printf("Triangle with base and altitude %f,%f of area is of %f \n",t[i].base,t[i].altitude,t[i].area);
     printf("The smallest triangle is the triangle with %f base and %f altitude \n",smallest.base,smallest.altitude);
 }
+
 int main()
+
 {
     int n ;
     n= input_n();
